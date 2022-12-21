ba3c931700a42541d284e06bad5eaece843172a1
Dilkash <dilkashpeshimam@gmail.com>
2022-12-21 20:48:57 +0530

    added sign up functionality using firebase rest authentication

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap'
-
+import axios from 'axios'
 
 const Signup = () => {
+    //AIzaSyBbr840gxPXx5wIRgO2KsmQYQpwHJKG91s
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const isPasswordConfirmed = () => {
+        if (password && confirmPassword && password === confirmPassword) return true;
+        return false
+    }
+    const handleSignUp = async (e) => {
+        e.preventDefault()
+        try {
+            if (!isPasswordConfirmed(password, confirmPassword)) {
+                alert('Password & confirm password should be same!')
+            } else {
+                if (email != '' && password != '' && confirmPassword != '') {
+                    const data = {
+                        email: email,
+                        password: password,
+                        returnSecureToken: true
+
+                    }
+                    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBbr840gxPXx5wIRgO2KsmQYQpwHJKG91s', data, {
+                        headers: {
+                            'Content-Type': 'application/json',
+
+                        }
+                    })
+
+                    setEmail('')
+                    setPassword('')
+                    setConfirmPassword('')
+
+                    alert('Signup Successful!')
+                }
+            }
+        } catch (err) {
+            console.log(err.message)
+            alert(err.response.data.error.message)
+        }
+    }
     return (
         <>
             <Container className='mt-5 ml-5 justify-content-center'>
@@ -12,18 +53,18 @@ const Signup = () => {
                         <Card className='shadow-lg'>
                             <Card.Header className='p-3 text-center display-6'>SIGN UP</Card.Header>
                             <Card.Body>
-                                <Form>
+                                <Form onSubmit={handleSignUp}>
                                     <Form.Group className='mb-3 p-1'>
                                         <Form.Label>Email</Form.Label>
-                                        <Form.Control type='email' placeholder='Enter email...'></Form.Control>
+                                        <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Enter email...' required></Form.Control>
                                     </Form.Group>
                                     <Form.Group className='mb-3 p-1'>
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control type='password' placeholder='Enter password...'></Form.Control>
+                                        <Form.Control value={password} onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Enter password...' required></Form.Control>
                                     </Form.Group>
                                     <Form.Group className='mb-3 p-1'>
                                         <Form.Label>Confirm Password</Form.Label>
-                                        <Form.Control type='password' placeholder='Confirm password...'></Form.Control>
+                                        <Form.Control value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type='password' placeholder='Confirm password...' required></Form.Control>
                                     </Form.Group>
                                     <Form.Group className='d-grid gap-2 mt-2' >
                                         <Button type='submit' size='lg'>SIGN UP</Button>
